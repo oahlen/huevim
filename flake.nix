8d033949b129405e36721f3edff6ffef1206f8cf
Oscar Ahlén <github@ahlen.dev>
2025-08-13 21:17:39 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,56 +3,44 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     crane.url = "github:ipetkov/crane";
-
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     nixpkgs,
-    rust-overlay,
     crane,
     flake-utils,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {inherit system overlays;};
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+        craneLib = crane.mkLib pkgs;
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-      src = craneLib.cleanCargoSource ./.;
-
-      nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
-
-      buildInputs = with pkgs; [openssl sqlite];
-
-      commonArgs = {
-        inherit src buildInputs nativeBuildInputs;
-      };
-
-      bin = craneLib.buildPackage (commonArgs
-        // {
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        });
-    in
-      with pkgs; {
-        packages.default = bin;
-
-        devShells.default = mkShell {
-          inputsFrom = [bin];
-          buildInputs = [rust-analyzer];
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = with pkgs; [pkg-config];
         };
-      })
-    // {
-      inherit nixpkgs;
-    };
+
+        bin = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
+      in
+        with pkgs; {
+          packages.default = bin;
+
+          devShells.default = craneLib.devShell {
+            buildInputs = [
+              clippy
+              rust-analyzer
+              rustfmt
+            ];
+          };
+        }
+    );
 }
