90355458660d9262320f77977a54f6bf2efc6caa
Oscar Ahlén <github@ahlen.dev>
2025-10-27 10:02:02 +0100

    Tweaks

--- src/args.rs
+++ src/args.rs
@@ -3,11 +3,11 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct Args {
-    /// The input colorscheme file
+    /// The input colorscheme file.
     pub filename: String,
-    /// Directory of generated colorscheme, default to the current working directory
+    /// Directory of generated colorscheme, default to the current working directory.
     pub output: Option<String>,
-    /// Overwrite the init.lua file if it already exists
+    /// Overwrite the init.lua file if it already exists.
     #[clap(long)]
     pub overwrite_init: bool,
 }
